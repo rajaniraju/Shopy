c0de4e65316d2ca04ffa7b3dd845cae51018ab60
rajaniraju <rajaniraju2003@gmail.com>
2022-02-05 19:44:59 -0600

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+  let headingText = "shoppingAppp";
+ 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       Let Us shop here
+       </header>
+       <body>
+       <div>{headingText}</div>
+       <button >ADD</button>
+       </body>
+       
     </div>
   );
 }
